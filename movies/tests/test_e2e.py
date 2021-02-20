c3fda3a73e0ff326def5eb2670d358ccfe4b1bfe
sheac <shealen.clare@gmail.com>
2021-02-20 08:43:38 -0800

    Adapt tests for caching and add cache test

--- movies/tests/test_e2e.py
+++ movies/tests/test_e2e.py
@@ -1,11 +1,15 @@
 import httpretty
 from django.test import TestCase, Client
+from django.core.cache import cache
+
 
 from ..clients import GhibliClient
 from ..config import GHIBLI_URL
 
 
 class TestE2E(TestCase):
+    def tearDown(self):
+        cache.clear()
 
     @httpretty.activate
     def test_movies_smoke(self):
@@ -70,3 +74,27 @@ class TestE2E(TestCase):
         self.assertEqual(response.status_code, 500)
         response_content = response.content.decode("utf-8")
         self.assertIn("internal server error", response_content)
+
+    @httpretty.activate
+    def test_movies_cache_response(self):
+        throwaway_client = GhibliClient(GHIBLI_URL)
+
+        httpretty.register_uri(
+            httpretty.GET,
+            throwaway_client.films_url(),
+            body="[]"
+        )
+        response = Client().get("/movies/")
+
+        # should return a 200 as usual
+        self.assertEqual(response.status_code, 200)
+
+        httpretty.register_uri(
+            httpretty.GET,
+            throwaway_client.films_url(),
+            status=404,
+        )
+        response = Client().get("/movies/")
+
+        # would return a 500 if the response wasn't cached, but will return a 200 because of caching
+        self.assertEqual(response.status_code, 200)
